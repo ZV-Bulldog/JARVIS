ec2ef350f1e182de1e42d3d253891f9a52a86df4
Zac Vickers <vick4393@kettering.edu>
2024-09-23 14:29:25 -0400

    Update voice_interaction.py

--- Configuration/voice_interaction.py
+++ Configuration/voice_interaction.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import pyttsx3
+from Configuration.config import ASSISTANT_IDENTITY
 
 def take_command():
     recognizer = sr.Recognizer()
@@ -17,7 +18,28 @@ def take_command():
             command = ""
     return command
 
-def talk(text):
+#def talk(text):
+    #engine = pyttsx3.init()
+    #engine.say(text)
+    #engine.runAndWait()
+
+def talk(text, voice_id=None):
     engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+
+    # Set the desired voice using the voice_id or fallback to a default
+    if voice_id is not None:
+        engine.setProperty('voice', voice_id)
+    else:
+        # You can set the default voice here by selecting from the list
+        engine.setProperty('voice', voices[0].id)  # Use the first available voice by default
+    # Slow down the speaking rate (default is around 200)
+    rate = engine.getProperty('rate')  # Get the current rate
+    engine.setProperty('rate', 192)  # Decrease rate by 50 (adjust as needed)
     engine.say(text)
     engine.runAndWait()
+
+def explain_virtual_assistant():
+    """Explains who the virtual assistant is."""
+    talk(ASSISTANT_IDENTITY)  # Use the variable from config.py
+    print(ASSISTANT_IDENTITY)  # Log the response
